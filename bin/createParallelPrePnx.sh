ca90cf46787f9367eca7bb94df3dee0902142f67
Tom Vanmechelen <tom.vanmechelen@libis.be>
2023-05-31 09:32:24 +0200

    cleanup code

--- bin/createParallelPrePnx.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-docker-compose run --rm lirias_collector bash -c "cd /app/src/; ruby collector.rb rules/lirias_parallel_rules.rb" >> ../logs/lirias_collector_parallel.log 2>&1
-
-
-
---
+++ bin/createPrePnx_develop.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+docker-compose run --rm --build lirias_collector_develop bash 
+
+
+
+
